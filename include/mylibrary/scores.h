85ff983200aafbb32d4619d343fd2800285fdf3f
Praval Telagi <praval.telagi@gmail.com>
2020-04-21 15:32:38 -0500

    Setup SQL database

---
+++ include/mylibrary/scores.h
@@ -0,0 +1,22 @@
+//
+// Created by Praval Telagi on 4/21/20.
+//
+
+#ifndef FINALPROJECT_SCORES_H
+#define FINALPROJECT_SCORES_H
+
+#include <sqlite_modern_cpp.h>
+
+namespace myapp {
+
+
+
+
+
+
+
+}
+
+
+
+#endif  // FINALPROJECT_SCORES_H
---
+++ src/scores.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Praval Telagi on 4/21/20.
+//
+
+#include <sqlite_modern_cpp.h>
+
+namespace myapp {
+
+
+
+
+
+
+
+
+}
