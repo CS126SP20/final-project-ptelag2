f0367dcd7008404914c864864d1a1b2e1e9d45e7
Praval Telagi <praval.telagi@gmail.com>
2020-04-25 17:22:03 -0500

    Added ability to add new floors to the game

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -24,11 +24,20 @@ MyApp::MyApp() : engine{},
 void MyApp::setup() {
   cinder::gl::color(0, 0, 1);
   engine.PlaceBlockOnLowestSurface();
+  engine.AddInitialFloors();
+  //engine.GetOpenFloor().pop_back();
+  //engine.PlaceBlockOnLowestSurface();
+  engine.PlaceBlockOnLowestSurface();
 }
 
 void MyApp::update() {
   if (engine.GetOpenFloor().empty()) {
     //leaderboard.InsertScoreToLeaderboard(engine.GetScore());
+  }
+
+  if (engine.GetFloors().size() <= 7) {
+    //engine.GetOpenFloor().insert(engine.GetOpenFloor().begin(), 5);
+    engine.AddRandomFloor();
   }
 }
 
@@ -51,30 +60,30 @@ void MyApp::DrawBlock() {
                                   + kBlockSize,
                                   kBlockSize * engine.GetBlock().GetYPosition()
                                   + kBlockSize));
+  /*if (engine.GetOpenFloor().size() <= 7) {
+    //engine.GetOpenFloor().insert(engine.GetOpenFloor().begin(), 5);
+    //engine.AddRandomFloor();
+  }*/
 }
 
 auto starting_time = std::chrono::system_clock::now();
-double height = 800;
+double height = 0;
+double speed = 5;
 void MyApp::DrawFloors() {
 
   //vector<int> vector{0, 1, 2, 3, 4, 19, 15, 6, 7, 13, 1, 8};
   auto current_time = std::chrono::system_clock::now();
   double current_delay =
       duration_cast<milliseconds>(current_time - starting_time).count() / 1000.0;
-  if (current_delay > 5) {
-    height-= 0.5;
+  if (current_delay > 10) {
+    height+= speed;
     //engine.GetBlock().SetYBlock(engine.GetBlock().GetYBlock() - 1);
     cinder::gl::color(0, 1, 0);
-    /*cinder::gl::drawSolidRect(Rectf(kBlockSize * engine.GetBlock().GetXPosition(),
-                                    kBlockSize * engine.GetBlock().GetYPosition() - 1,
-                                    kBlockSize * engine.GetBlock().GetXPosition()
-                                    + kBlockSize,
-                                    kBlockSize * engine.GetBlock().GetYPosition()
-                                    + kBlockSize - 1));*/
-    engine.GetBlock().SetYPosition(engine.GetBlock().GetYPosition() - (0.50/40.0));
+
+    engine.GetBlock().SetYPosition(engine.GetBlock().GetYPosition() - (speed/40.0));
     //starting_time = std::chrono::system_clock::now();
   }
-  vector<int> vector = engine.GetOpenFloor();
+  /*vector<int> vector = engine.GetOpenFloor();
   cinder::gl::color(1, 1, 0);
   for (int i = vector.size() - 1; i >= 0; i--) {
     cinder::gl::drawSolidRect(Rectf(0,
@@ -86,6 +95,18 @@ void MyApp::DrawFloors() {
                                     height - (2 * i * kHeightOfFloor),
                                     800,
                                     height - (2 * i * kHeightOfFloor) - kHeightOfFloor));
+  }*/
+  cinder::gl::color(1, 1, 0);
+  for (int i = 0; i < engine.GetFloors().size(); i++) {
+    cinder::gl::drawSolidRect(Rectf(0,
+                                    engine.GetFloors()[i].GetHeight() * 40 - height,
+                                    (engine.GetFloors()[i].GetOpenSpot()) * kHeightOfFloor,
+                                    engine.GetFloors()[i].GetHeight() * 40 + kHeightOfFloor - height));
+
+    cinder::gl::drawSolidRect(Rectf((engine.GetFloors()[i].GetOpenSpot() + 1) * kHeightOfFloor,
+                                    engine.GetFloors()[i].GetHeight() * 40 - height,
+                                    800,
+                                    engine.GetFloors()[i].GetHeight() * 40 + kHeightOfFloor - height));
   }
 }
 
