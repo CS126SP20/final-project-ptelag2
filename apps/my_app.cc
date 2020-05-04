7f28dfa203461065fc2d5f2f192ca60e1e63e277
Praval Telagi <praval.telagi@gmail.com>
2020-05-04 10:37:49 -0500

    Added welcome screen and moved variables to a more organized location

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -9,6 +9,7 @@
 namespace myapp {
 
 using cinder::app::KeyEvent;
+using cinder::app::MouseEvent;
 using cinder::Rectf;
 using cinder::Color;
 using cinder::ColorA;
@@ -20,8 +21,6 @@ using cinder::TextBox;
 using std::chrono::milliseconds;
 
 double RGB_colors[]{1, 1, 0};
-auto game_starting_time = std::chrono::system_clock::now();
-auto starting_time = std::chrono::system_clock::now();
 
 MyApp::MyApp() : engine{},
                    leaderboard{cinder::app::getAssetPath(kDbPath).string()} {}
@@ -30,42 +29,54 @@ void MyApp::setup() {
   height_offset = 0;
   speed_can_increase = false;
   added_score_to_table = false;
+  on_welcome_screen = true;
+  game_started = false;
+  timers_have_been_set = false;
+
+  game_starting_time = std::chrono::system_clock::now();
+  starting_time = std::chrono::system_clock::now();
 
   engine.GetBlock().SetXPosition(0);
-  engine.AddInitialFloors();
+  engine.AddRandomInitialFloors();
   engine.PlaceBlockOnLowestSurface();
-  engine.SetGameMode(kEasy);
 }
 
 
-
 void MyApp::update() {
+  if (game_started) {
 
-  auto current_time = std::chrono::system_clock::now();
-  double animation_start_delay =
-      duration_cast<milliseconds>(current_time - game_starting_time).count() / 1000.0;
-
-  double speed_increase_delay =
-      duration_cast<milliseconds>(current_time - starting_time).count() / 1000.0;
-
-  if (speed_increase_delay > 5 && speed_can_increase) {
-    engine.IncreaseSpeed(0.05);
-    starting_time = std::chrono::system_clock::now();
-    RGB_colors[kRed] = (float)rand() / (RAND_MAX);
-    RGB_colors[kGreen] = (float)rand() / (RAND_MAX);
-    RGB_colors[kBlue] = (float)rand() / (RAND_MAX);
-  }
-
-  if (animation_start_delay > 10) {
-    height_offset+= engine.GetSpeed();
-    engine.MoveBlockUp();
-    if (!speed_can_increase) {
+    if (!timers_have_been_set) {
+      game_starting_time = std::chrono::system_clock::now();
       starting_time = std::chrono::system_clock::now();
+      timers_have_been_set = true;
     }
-    speed_can_increase = true;
+
+    auto current_time = std::chrono::system_clock::now();
+    double animation_start_delay =
+        duration_cast<milliseconds>(current_time - game_starting_time).count() /
+        1000.0;
+
+    double speed_increase_delay =
+        duration_cast<milliseconds>(current_time - starting_time).count() /
+        1000.0;
+
+    if (speed_increase_delay > 5 && speed_can_increase) {
+      engine.IncreaseSpeed(0.05);
+      starting_time = std::chrono::system_clock::now();
+      RGB_colors[kRed] = (float)rand() / (RAND_MAX);
+      RGB_colors[kGreen] = (float)rand() / (RAND_MAX);
+      RGB_colors[kBlue] = (float)rand() / (RAND_MAX);
+    }
+
+    if (animation_start_delay > 10) {
+      height_offset += engine.GetSpeed();
+      engine.MoveBlockUp();
+      if (!speed_can_increase) {
+        starting_time = std::chrono::system_clock::now();
+      }
+      speed_can_increase = true;
+    }
   }
-
-
 
   if (engine.GetFloors().size() <= engine.GetFloorGeneratorOffset()) {
     engine.AddRandomFloor();
@@ -78,17 +89,24 @@ void MyApp::update() {
 }
 
 void MyApp::draw() {
+
+  if (on_welcome_screen) {
+    DrawWelcomeScreen();
+    return;
+  }
+
   if (engine.IsGameOver()) {
     DrawGameOver();
     return;
   }
+
   DrawBackground();
   DrawFloors();
   DrawBlock();
 }
 
 void MyApp::DrawBackground() {
-  cinder::gl::clear(Color(1, 1, 0));
+  cinder::gl::clear(Color(0, 0, 0));
 }
 
 void MyApp::DrawBlock() {
@@ -103,7 +121,7 @@ void MyApp::DrawBlock() {
 }
 
 void MyApp::DrawFloors() {
-  cinder::gl::color(RGB_colors[0], RGB_colors[1], RGB_colors[2]);
+  cinder::gl::color(RGB_colors[kRed], RGB_colors[kGreen], RGB_colors[kBlue]);
   for (int i = 0; i < engine.GetFloors().size(); i++) {
     cinder::gl::drawSolidRect(Rectf(0,
                                     engine.GetFloors()[i].GetHeight() * kHeightOfFloor - height_offset,
@@ -135,6 +153,42 @@ void PrintText(const string& text, const C& color, const cinder::ivec2& size,
   const auto surface = box.render();
   const auto texture = cinder::gl::Texture::create(surface);
   cinder::gl::draw(texture, locp);
+}
+
+
+
+void MyApp::DrawWelcomeScreen() {
+  cinder::gl::clear(Color(0, 0, 0));
+  const cinder::vec2 center = getWindowCenter();
+  const cinder::ivec2 size = {500, 50};
+  PrintText("Escape the Ceiling", Color::white(), size, {center.x, center.y - 250});
+  PrintText("Choose Mode: ", Color::white(), size, {center.x, center.y - 150});
+  PrintText("Easy", Color::white(), size, {center.x, center.y - 100});
+  PrintText("Medium", Color::white(), size, {center.x, center.y - 50});
+  PrintText("Hard", Color::white(), size, {center.x, center.y});
+
+  cinder::gl::color(RGB_colors[kRed], RGB_colors[kGreen], RGB_colors[kBlue]);
+  for (int i = 0; i < cosmetic_floors.size(); i++) {
+    cinder::gl::drawSolidRect(Rectf(0,
+                                    800 - (2 * i * 40),
+                                    (cosmetic_floors[i]) * kHeightOfFloor,
+                                    800 - (2 * i * 40) - 40));
+
+    cinder::gl::drawSolidRect(Rectf((cosmetic_floors[i] + 1) * kHeightOfFloor,
+                                    800 - (2 * i * 40),
+                                    800,
+                                    800 - (2 * i * 40) - 40));
+  }
+
+  cinder::gl::color(0, 1, 0);
+  cinder::gl::drawSolidRect(Rectf(kBlockSize * 2,
+                                  kBlockSize * 10,
+                                  kBlockSize * 2
+                                  + kBlockSize,
+                                  kBlockSize * 10
+                                  + kBlockSize));
+
+
 }
 
 void MyApp::DrawGameOver() {
@@ -175,9 +229,10 @@ void MyApp::PrintTopScores() {
 
   vector<int> top_scores = leaderboard.GetHighestScores(3, engine.GetGameMode());
   int row = 2;
+  int score_rank = 1;
   for (const int score : top_scores) {
     std::stringstream ss;
-    ss << score;
+    ss << score_rank++ << ". " << score;
     PrintText(ss.str(), Color::black(), size, {center.x, center.y + (++row * 50)});
   }
 }
@@ -194,7 +249,28 @@ void MyApp::keyDown(KeyEvent event) {
       engine.GetBlock().MoveRight();
       break;
     }
+    case KeyEvent::KEY_ESCAPE: {
+      engine.ForceGameOver();
+      break;
+    }
   }
 }
+
+void MyApp::mouseDown(MouseEvent event) {
+  if (event.isLeft() && on_welcome_screen) {
+    if (event.getX() >= 345 && event.getX() <= 450) {
+      if (event.getY() >= 280 && event.getY() <= 320) {
+        engine.SetGameMode(kEasy);
+      } else if (event.getY() >= 320 && event.getY() <= 375) {
+        engine.SetGameMode(kMedium);
+      } else if (event.getY() >= 375 && event.getY() <= 415) {
+        engine.SetGameMode(kHard);
+      }
+      on_welcome_screen = false;
+      game_started = true;
+    }
+  }
+}
+
 
 }  // namespace myapp
--- apps/my_app.h
+++ apps/my_app.h
@@ -7,8 +7,11 @@
 #include <mylibrary/engine.h>
 #include <mylibrary/scores.h>
 
+#include <chrono>
+
+using std::chrono::system_clock;
+
 const int kBlockSize = 40;
-const int kHeightOfFloor = 40;
 const char kDbPath[] = "leaderboard.db";
 const char kNormalFont[] = "Arial";
 const double kEasy = 1.0;
@@ -17,6 +20,7 @@ const double kHard = 2.0;
 const int kRed = 0;
 const int kGreen = 1;
 const int kBlue = 2;
+const vector<int> cosmetic_floors{19, 6, 13, 3, 1};
 
 namespace myapp {
 
@@ -27,20 +31,60 @@ class MyApp : public cinder::app::App {
   void update() override;
   void draw() override;
   void keyDown(cinder::app::KeyEvent) override;
+  void mouseDown(cinder::app::MouseEvent) override;
+
+  /**
+   * Draws the background for the game
+   */
   void DrawBackground();
+
+  /**
+   * Draws the Block the user controls
+   */
   void DrawBlock();
+
+  /**
+   * Draws all the floors in present in the engine
+   */
   void DrawFloors();
+
+  /**
+   * Draws the Game over screen
+   */
   void DrawGameOver();
+
+  /**
+   * Draws the welcome screen
+   */
+  void DrawWelcomeScreen();
+
+  /**
+   * Prints the game mode as text
+   */
   void PrintGameMode();
+
+  /**
+   * Prints the user's score as text
+   */
   void PrintPlayerScore();
+
+  /**
+   * Prints the Top scores in the leaderboard as text
+   */
   void PrintTopScores();
 
  private:
   SQL_Leaderboard leaderboard;
   Engine engine;
+  std::chrono::time_point<system_clock> game_starting_time;
+  std::chrono::time_point<system_clock> starting_time;
+
   double height_offset;
   bool speed_can_increase;
   bool added_score_to_table;
+  bool on_welcome_screen;
+  bool game_started;
+  bool timers_have_been_set;
 };
 
 }  // namespace myapp
